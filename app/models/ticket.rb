3c3eb2a631ad38bf38a54c030362317bd335294c
Valery <valeryhx@gmail.com>
2016-06-04 15:00:03 +0300

    Authorized users can now leave comments on tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :author, class_name: "User"
+
+  has_many   :comments, dependent: :destroy
   has_many   :attachments, dependent: :destroy
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
 
